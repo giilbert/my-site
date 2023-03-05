3803d3c84222c24887ceb3ea4cda4055079e4073
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-03-05 14:07:16 -0500

    fix project card hover effects

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 export const ProjectCard: React.FC = () => {
   return (
     <motion.div
-      className="bg-neutral-500/10 rounded-md cursor-pointer transform hover:scale-[102%] transition-transform hover:ring-1 ring-gray-700"
       variants={{
         hidden: {
           opacity: 0,
@@ -20,16 +19,18 @@ export const ProjectCard: React.FC = () => {
         },
       }}
     >
-      <AspectRatio
-        ratio={16 / 9}
-        className="bg-neutral-400/10 rounded-tl-md rounded-tr-md flex justify-center items-center"
-      >
-        <p>Insert image here</p>
-      </AspectRatio>
+      <div className="bg-neutral-500/10 rounded-md cursor-pointer transform hover:scale-[102%] transition-transform hover:ring-1 ring-gray-700">
+        <AspectRatio
+          ratio={16 / 9}
+          className="bg-neutral-400/10 rounded-tl-md rounded-tr-md flex justify-center items-center"
+        >
+          <p>Insert image here</p>
+        </AspectRatio>
 
-      <div className="px-3 py-2">
-        <h3 className="text-xl">Insert title here</h3>
-        <p className="text-sm text-white/70">Insert short description here</p>
+        <div className="px-3 py-2">
+          <h3 className="text-xl">Insert title here</h3>
+          <p className="text-sm text-white/70">Insert short description here</p>
+        </div>
       </div>
     </motion.div>
   );
