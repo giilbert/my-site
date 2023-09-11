a7ceb06b279afc5b5fb3c6fb2ee9794b9cc4d336
gil <40220369+giilbert@users.noreply.github.com>
2023-09-11 00:44:20 +0000

    chore: update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  experimental: {
-    assets: true
-  },
-  integrations: [mdx(), sitemap(), tailwind(), prefetch(), solidJs()]
+  integrations: [mdx(), sitemap(), tailwind(), prefetch(), solidJs()],
 });
