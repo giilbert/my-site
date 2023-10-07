178ef408d741d8b236f7c2c83ffc851d21ae1aeb
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-10-07 19:45:42 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), prefetch(), solidJs()],
 });
